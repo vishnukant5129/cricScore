416f45c1625a1cf7837644fd98c845dc6d9493fb
vishnukant <vishnukantray10@gmail.com>
2026-06-06 17:23:18 +0530

    add JWT auth middleware to protected profile route

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -5,12 +5,13 @@ import {
     login,
     logout
 } from "../controllers/authController.js";
+import authMiddleware from "../middleware/authMiddleware.js"
 
 const router = express.Router();
 
 router.post("/signup", signup);
 // router.post("/verify-otp", verifyOTP);
 router.post("/login", login);
-router.post("/logout", logout);
+router.post("/logout", authMiddleware, logout);
 
 export default router;
--- backend/src/routes/player.routes.js
+++ backend/src/routes/player.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { getProfile } from "../controllers/player.controller.js";
+import authMiddleware from '../middleware/authMiddleware.js'
 
 const router = express.Router();
 
-router.get("/profile", getProfile);
+router.get("/profile", authMiddleware, getProfile);
 
 export default router;
