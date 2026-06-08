38515c0ccb90e762d8819c49424ab74a58d0097b
vishnukant <vishnukantray10@gmail.com>
2026-06-08 13:17:07 +0530

    backend fectch data from teams api

--- frontend/src/components/selectTeam/SelectTeam.jsx
+++ frontend/src/components/selectTeam/SelectTeam.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
+import api from "../../api/api.js";
 
 const SelectTeam = () => {
+    const [data, setData] = useState([]);
+
+    const fetchTeams = async () => {
+        try {
+            const res = await api.get(
+                "/match/teams"
+            );
+
+            setData(res.data.data);
+            console.log(res.data.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
     return (
         <div>
-            
-        </div>
-    )
-}
+            <button onClick={fetchTeams}>Fetch Teams</button>
 
-export default SelectTeam
+            {data.map((team) => (
+                <div key={team._id}>
+                    <h3>{team.name}</h3>
+                    <p>{team.location}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default SelectTeam;
