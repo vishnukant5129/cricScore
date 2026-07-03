72cdfb0e9db69be8887add879cbecd55779fa00e
vishnukant <vishnukantray10@gmail.com>
2026-07-03 14:41:59 +0530

    backend: create all the controller files

--- backend/src/controllers/auth.Controller.js
+++ backend/src/controllers/auth.Controller.js
@@ -3,15 +3,6 @@ import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { uploadToCloudinary } from "../utils/cloudinaryUpload.js";
 
-// Generate OTP
-// const generateOTP = () => {
-//     return Math.floor(
-//         100000 + Math.random() * 900000
-//     ).toString();
-// };
-
-
-
 // ================= SIGNUP + SEND OTP =================
 export const signup = async (req, res) => {
     try {
@@ -49,21 +40,6 @@ export const signup = async (req, res) => {
             });
         }
 
-        // if (
-        //     !fullname ||
-        //     !mobilenumber ||
-        //     !password ||
-        //     !location ||
-        //     !gender ||
-        //     !dob
-        // ) {
-        //     return res.status(400).json({
-        //         success: false,
-        //         message: "All fields required",
-        //     });
-        // }
-
-        // Check existing user
         let user = await User.findOne({
             mobilenumber
         });
@@ -81,21 +57,6 @@ export const signup = async (req, res) => {
                 10
             );
 
-        // OTP
-        // const otp = generateOTP();
-
-        // const otpExpiry =
-        //     Date.now() + 5 * 60 * 1000;
-
-        // Profile Photo URL
-        // let profilePhoto = "";
-
-        // if (req.file) {
-        //     // Cloudinary URL here
-        //     profilePhoto = req.file.path;
-        // }
-
-        // Create / Update user
         if (!user) {
             user = await User.create({
                 fullname,
@@ -105,8 +66,6 @@ export const signup = async (req, res) => {
                 gender,
                 dob,
                 profilePhoto: profilePictureUrl,
-                // otp,
-                // otpExpiry,
                 isVerified: true,
             });
         }
@@ -123,22 +82,6 @@ export const signup = async (req, res) => {
             },
         });
 
-        // else {
-        //     user.otp = otp;
-        //     user.otpExpiry = otpExpiry;
-        //     await user.save();
-        // }
-
-        // TODO:
-        // Send OTP via Twilio / MSG91
-        // console.log("OTP:", otp);
-
-        // res.status(200).json({
-        //     success: true,
-        //     message:
-        //         "OTP sent to mobile number",
-        // });
-
     } catch (error) {
         res.status(500).json({
             success: false,
@@ -146,118 +89,6 @@ export const signup = async (req, res) => {
         });
     }
 };
-
-
-
-// ================= VERIFY OTP =================
-// export const verifyOTP = async (
-//     req,
-//     res
-// ) => {
-//     try {
-
-//         const {
-//             mobilenumber,
-//             otp
-//         } = req.body;
-
-//         if (
-//             !mobilenumber ||
-//             !otp
-//         ) {
-//             return res.status(400).json({
-//                 success: false,
-//                 message:
-//                     "Mobile and OTP required",
-//             });
-//         }
-
-//         const user =
-//             await User.findOne({
-//                 mobilenumber
-//             });
-
-//         if (!user) {
-//             return res.status(404).json({
-//                 success: false,
-//                 message:
-//                     "User not found",
-//             });
-//         }
-
-//         // OTP check
-//         if (
-//             user.otp !== otp ||
-//             user.otpExpiry < Date.now()
-//         ) {
-//             return res.status(400).json({
-//                 success: false,
-//                 message:
-//                     "Invalid or expired OTP",
-//             });
-//         }
-
-//         // Verified
-//         user.isVerified = true;
-//         user.otp = null;
-//         user.otpExpiry = null;
-
-//         await user.save();
-
-//         // JWT
-//         const token = jwt.sign(
-//             {
-//                 id: user._id,
-//             },
-//             process.env.JWT_SECRET,
-//             {
-//                 expiresIn: "7d",
-//             }
-//         );
-
-//         res
-//             .cookie(
-//                 "token",
-//                 token,
-//                 {
-//                     httpOnly: true,
-//                     secure: false,
-//                     sameSite:
-//                         "strict",
-//                     maxAge:
-//                         7 *
-//                         24 *
-//                         60 *
-//                         60 *
-//                         1000,
-//                 }
-//             )
-//             .status(200)
-//             .json({
-//                 success: true,
-//                 message:
-//                     "OTP verified successfully",
-//                 token,
-//                 user: {
-//                     id: user._id,
-//                     fullname:
-//                         user.fullname,
-//                     mobilenumber:
-//                         user.mobilenumber,
-//                     profilePhoto:
-//                         user.profilePhoto,
-//                 },
-//             });
-
-//     } catch (error) {
-//         res.status(500).json({
-//             success: false,
-//             message: error.message,
-//         });
-//     }
-// };
-
-
 
 // ================= LOGIN =================
 export const login = async (req, res) => {
@@ -347,15 +178,11 @@ export const login = async (req, res) => {
 
 
 // ================= LOGOUT =================
-export const logout = async (
-    req,
-    res
-) => {
-
+export const logout = async (req, res) => {
     res
         .clearCookie("token", {
-            httpOnly: true, 
-            sameSite: 'lax', 
+            httpOnly: true,
+            sameSite: 'lax',
             secure: false
         })
         .status(200)
@@ -364,4 +191,17 @@ export const logout = async (
             message:
                 "Logout successful",
         });
+};
+
+export default refreshToken = async (req, res) => {
+};
+
+
+export default forgotPassword = async (req, res) => {
+};
+
+export default verifyEmail = async (req, res) => {
+};
+
+export default getProfile = async (req, res) => {
 };
